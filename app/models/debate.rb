57f800cb3d853102c14bd590d999be3f2cf4191f
Raghu Betina <raghu@firstdraft.com>
2017-03-14 10:19:32 +0000

    Direct association between users and debates

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,6 +1,8 @@
 class Debate < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :debates,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
